e602efa2be3ecf7e6780b45a272c23517bdbd7fc
trung-vt <trung-vt>
2024-06-03 15:55:54 +0000

    [Old] Modify dataset.py

--- data/static_img/dataset.py
+++ data/static_img/dataset.py
@@ -6,7 +6,7 @@ import numpy as np
 from torch.utils.data import Dataset
 from PIL import Image
 
-from .data_utils import extract_patches_3d
+from .data_utils import extract_patches_2d
 
 
 class StaticImageDenoisingDataset(Dataset):
@@ -19,7 +19,7 @@ class StaticImageDenoisingDataset(Dataset):
 		sigma=0.23,  
 		patches_size = None,
 		strides= None,
-		extract_data=True,
+		force_recreate_data=True,
 		device: str = "cuda"
 	):
 		self.device = device
@@ -28,23 +28,37 @@ class StaticImageDenoisingDataset(Dataset):
 		ids = [str(x).zfill(2) for x in ids]
 
 		xf_list = []
-  
-		for k, img_id in enumerate(ids):
+
+		k = 0
+		for folder in os.listdir(data_path):
+			# The first 4 characters of folder name is the image id
+			img_id = folder[:4]
+			if img_id not in ids:
+				continue
+			k += 1
 			print(f'loading image id {img_id}, {k}/{len(ids)}')
-			# TODO: Choose sample path
-			# sample_path = os.path.join(data_path, f"MOT17-{img_id}")
-			sample_path = os.path.join(data_path, f"Static-Images-{img_id}")
-			if extract_data:
-				xf = self.create_static_img(sample_path).unsqueeze(0) / 255
-			else:
-				scale_factor_str = str(self.scale_factor).replace('.','_')
-				xf = np.load(os.path.join(sample_path, f"xf_scale_factor{scale_factor_str}.npy"))
+
+			sample_path = os.path.join(data_path, folder)
+			scale_factor_str = str(self.scale_factor).replace('.','_')
+			npy_file = f"xf_scale_factor{scale_factor_str}.npy"
+
+			# if file exists
+			if os.path.exists(npy_file) and not force_recreate_data:
+				xf = np.load(npy_file)
 				xf = torch.tensor(xf, dtype=torch.float)
-				xf = xf.unsqueeze(0).unsqueeze(0) / 255
-			
+				xf = xf.unsqueeze(0) #TODO: Do we need to unsqueeze here when we will unsqueeze again later???
+			else:
+				xf = self.create_static_img(sample_path)
+				np.save(os.path.join(sample_path, npy_file), xf)
+
+			xf = xf.unsqueeze(0) / 255
+
+			print(f"xf shape: {xf.shape}")
+
 			if patches_size is not None:
 				print(f"extracting patches of shape {patches_size}; strides {strides}")
-				xfp = extract_patches_3d(xf.contiguous(), patches_size, stride=strides)
+				xfp = extract_patches_2d(xf.contiguous(), patches_size, stride=strides)
+				print(f"xfp shape: {xfp.shape}")
 				xf_list.append(xfp)
 			
 		if patches_size is not None:
@@ -53,16 +67,16 @@ class StaticImageDenoisingDataset(Dataset):
 			xf = torch.concat(xf_list,dim=0)
 			
 		else:
-			xf = xf.unsqueeze(0)
+			xf = xf.unsqueeze(0) # TODO: Why do we need to unsqueeze again???
 		
-		# #create temporal TV vector to detect which patches contain the most motion
-		# xfp_tv = (xf[...,1:] - xf[...,:-1]).pow(2).sum(dim=[1,2,3,4]) #contains the TV for all patches
+		#create temporal TV vector to detect which patches contain the most motion
+		xfp_tv = (xf[...,1:] - xf[...,:-1]).pow(2).sum(dim=[1,2,3,4]) #contains the TV for all patches
 		
-		# #normalize to 1 to have a probability vector
-		# xfp_tv /= torch.sum(xfp_tv)
+		#normalize to 1 to have a probability vector
+		xfp_tv /= torch.sum(xfp_tv)
 		
-		# #sort TV in descending order --> xfp_tv_ids[0] is the index of the patch with the most motion
-		# self.samples_weights = xfp_tv
+		#sort TV in descending order --> xfp_tv_ids[0] is the index of the patch with the most motion
+		self.samples_weights = xfp_tv
 		
 		self.xf = xf
 		self.len = xf.shape[0]
@@ -79,29 +93,24 @@ class StaticImageDenoisingDataset(Dataset):
 		else:
 			raise ValueError("Invalid sigma value provided, must be float, tuple or list.")
 
-	def create_static_img(self, sample_path: str):
+	def create_static_img(self, files_path: str):
 		
-		files_path = os.path.join(sample_path, "img1")
-		files_list = os.listdir(files_path)
 		xf = []
 
-		for file in files_list:
-			
-			image = Image.open(os.path.join(files_path, file))
-			
-			#resize
-			Nx_,Ny_ = np.int(np.floor(self.scale_factor * image.width )), np.int(np.floor(self.scale_factor * image.height ))
-			image = image.resize( (Nx_, Ny_) )
-			
-			#convert to grey_scale
-			image = image.convert('L')
-			image_data = np.asarray(image)
-			xf.append(image_data)
+		file_name = "GT_SRGB_010.PNG"
+		image = Image.open(os.path.join(files_path, file_name))
+		
+		#resize
+		Nx_,Ny_ = np.int(np.floor(self.scale_factor * image.width )), np.int(np.floor(self.scale_factor * image.height ))
+		image = image.resize( (Nx_, Ny_) )
+		
+		#convert to grey_scale
+		image = image.convert('L')
+		image_data = np.asarray(image)
+		xf.append(image_data)
 			
 		xf = np.stack(xf, axis=-1)
-		
-		scale_factor_str = str(self.scale_factor).replace('.','_')
-		np.save(os.path.join(sample_path, f"xf_scale_factor{scale_factor_str}.npy"), xf)
+		print(f"xf shape: {xf.shape}")
 		
 		return torch.tensor(xf, dtype = torch.float)
 			

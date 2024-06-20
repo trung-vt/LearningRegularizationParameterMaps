a49dc9c94a90c41aeecaa05e71dd360ea413315b
trung-vt <trung-vt>
2024-06-20 03:32:09 -0500

    install local modules

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+   name='learning_regularisation_parameter_map',
+   version='1.0',
+   description='Learn regularisation parameter map for image reconstruction using deep learning.',
+   author='trung-vt',
+   author_email='trung.vuthanh24@gmail.com',
+   packages=['networks'],
+#    packages=['learning_regularisation_parameter_map'], #same as name
+#    install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+   install_requires=['wheel', 'torch'], #external packages as dependencies
+    # scripts=[
+    #     'networks/pdhg',
+    # ]
+)

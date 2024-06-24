247951e8d491df219bb36fb46de67e4479f4d982
trung-vt <trung-vt>
2024-06-23 21:20:17 -0500

    upd turtle id data loading module

--- data/turtle_data_loading.py
+++ data/turtle_data_loading.py
@@ -1,80 +1,117 @@
 
 import torch
-import os
 from PIL import Image
 import numpy as np
 from tqdm import tqdm
 
 from data.transform import convert_to_tensor_4D
 from data.turtle_data_generate import TurtleDataGenerator
+from config.load import get_sigmas
 
-def get_datasets(config):
-    data_path = config["dataset"]
-    with open(f"{data_path}/train.txt", "r") as f:
-        train_files = f.read().splitlines()
-    with open(f"{data_path}/val.txt", "r") as f:
-        val_files = f.read().splitlines()
-    with open(f"{data_path}/test.txt", "r") as f:
-        test_files = f.read().splitlines()
+
+def get_original_folder(size):
+    return f"images_crop_resize_{size}_greyscale"
+
+def get_noisy_folder_prefix(size):
+    return f"images_crop_resize_{size}_greyscale_noisy"
+
+def get_noisy_folder(prefix, sigma):
+    return f"{prefix}_{str(sigma).replace('.', '_')}"
+
+def get_file_paths(base_path:str, type:str, num_samples:int, sigmas:list, size=int):
+    """
+    
+    Parameters
+    ----------
+    num_samples : int
+        Number of samples to load. If 0, load all samples.
+    """
+    with open(f"{base_path}/{type}.txt", "r") as f:
+        files = f.read().splitlines()
+    if num_samples != 0:
+        files = files[:num_samples]
         
-    train_num_samples = config["train_num_samples"]
-    val_num_samples = config["val_num_samples"]
-    test_num_samples = config["test_num_samples"]
-    
-    train_files = train_files[:train_num_samples]
-    val_files = val_files[:val_num_samples]
-    test_files = test_files[:test_num_samples]
-    
+    file_paths = {}
+        
+    def load(sigma, folder):
+        file_paths[sigma] = []
+        for i in tqdm(range(len(files))):
+            file = files[i]
+            file_path = f"{folder}/{file}"
+            file_paths[sigma].append(file_path)   
+        
+    original_folder = get_original_folder(size)
+    print(f"Loading original image paths in {original_folder} ", end="")
+    load(0, original_folder)
+        
+    prefix = get_noisy_folder_prefix(size)
+    for sigma in sigmas:
+        noisy_folder = get_noisy_folder(prefix, sigma)
+        print(f"Loading noisy image paths sigma={sigma} in {noisy_folder} ", end="")
+        load(sigma, noisy_folder)
+
+    return file_paths
+
+
+
+def get_datasets(config, generating_data:bool=False, device="cuda"):
+    base_path = config["dataset"]
     size = config["resize_square"]
-    num_threads = config["data_gen_num_threads"]
-    sigmas = config["sigmas"]
-    # Convert string "[0.1, 0.15, 0.2, 0.25, 0.3]" to list [0.1, 0.15, 0.2, 0.25, 0.3]
-    sigmas = [float(sigma) for sigma in sigmas[1:-1].split(", ")]
+    sigmas = get_sigmas(config["sigmas"])
     
-    data_generator = TurtleDataGenerator(
-        turtle_data_path='../data/turtle_id_2022/turtles-data/data', 
-        size=size, num_threads=num_threads,
-        sigmas=sigmas,
-    )
-    data_generator.generate_cropped_and_resized_images()
+    if generating_data:
+        num_threads = config["data_gen_num_threads"]
+        data_generator = TurtleDataGenerator(
+            turtle_data_path=base_path, 
+            size=size, num_threads=num_threads,
+            sigmas=sigmas,
+        )
+        data_generator.generate_cropped_and_resized_images()
     
-    train_dataset = TurtleDataset(data_path, train_files, size=size)
-    val_dataset = TurtleDataset(data_path, val_files, size=size)
-    test_dataset = TurtleDataset(data_path, test_files, size=size)
-    
-    return train_dataset, val_dataset, test_dataset
+    def get_dataset(type):
+        num_samples = config[f"{type}_num_samples"]
+        file_paths = get_file_paths(base_path, type, num_samples, sigmas, size)
+        return TurtleDataset(base_path, file_paths, device=device)
+        
+    return get_dataset("train"), get_dataset("val"), get_dataset("test")
         
 
 class TurtleDataset(torch.utils.data.Dataset):
-    def get_image(self, img_path):
+    def get_img_4d(self, img_path):
         img = Image.open(img_path)
         img_np = np.array(img)
         img_4d = convert_to_tensor_4D(img_np)
         return img_4d
     
-    def __init__(self, data_path, files, sigmas=[0.1, 0.15, 0.2, 0.25, 0.3], size=512, device="cuda"):
-        self.originals = []
-        for file in files:
-            original_4d = self.get_image(f"{data_path}/images_crop_resize_{size}_greyscale/{file}")
-            self.originals.append(original_4d)
-        self.originals = torch.stack(self.originals, dim=0).to(device)
-            
-        prefix = f"images_crop_resize_{size}_greyscale_noisy"
-        self.images_pairs = []
-        for sigma in sigmas:
-            images_folder = f"{prefix}_{str(sigma).replace('.', '_')}"
-            print(f"Loading images from {images_folder}")
-            for i, file in tqdm(enumerate(files)):
-                noisy_4d = self.get_image(f"{data_path}/{images_folder}/{file}").to(device)
-                self.images_pairs.append((noisy_4d, i))
-                
-        self.device = device
-        
-    def __len__(self):
-        return len(self.images_pairs)
+    def load(self, base_path, file_paths, img_list):
+        for i in tqdm(range(len(file_paths))):
+            file_path = base_path + "/" + file_paths[i]
+            img_4d = self.get_img_4d(file_path).to(self.device)
+            img_list.append(img_4d)
     
+    def __init__(self, base_path, file_paths, device="cuda"):
+        self.device = device
+        sigmas = list(file_paths.keys())
+        assert len(sigmas) > 1, "At least original and one noisy image group is required."
+        assert sigmas[0] == 0, "First sigma must be 0 for original images."
+        
+        self.originals = []
+        original_file_paths = file_paths[0]
+        print(f"Loading original images ", end="")
+        self.load(base_path, original_file_paths, self.originals)
+        self.originals = torch.stack(self.originals, dim=0)
+
+        self.noisies = []
+        for j in range(1, len(sigmas)):
+            noisy_file_paths = file_paths[sigmas[j]]
+            print(f"Loading noisy images sigma={sigmas[j]} ", end="")
+            self.load(base_path, noisy_file_paths, self.noisies)
+        self.noisies = torch.stack(self.noisies, dim=0)
+
+
+    def __len__(self): return len(self.noisies)
+
     def __getitem__(self, idx):
-        noisy = self.images_pairs[idx][0]
-        clean_idx = self.images_pairs[idx][1]
-        clean = self.originals[clean_idx]
+        noisy = self.noisies[idx]
+        clean = self.originals[idx % len(self.originals)]
         return (noisy, clean)

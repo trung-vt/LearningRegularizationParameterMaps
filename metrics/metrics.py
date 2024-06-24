31ce6731dcc4da1d412232815e03e9483d6161ec
trung-vt <trung-vt>
2024-06-24 11:54:48 -0500

    mode compare function to metrics.py

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,5 +1,6 @@
 import torch
-from skimage.metrics import structural_similarity
+from skimage.metrics import structural_similarity, peak_signal_noise_ratio
+import numpy as np
 
 def PSNR(original, compressed): 
     mse = torch.mean((original - compressed) ** 2) 
@@ -20,3 +21,14 @@ def SSIM(tensor_2D_a: torch.Tensor, tensor_2D_b: torch.Tensor, data_range: float
         tensor_2D_a.to("cpu").detach().numpy(), 
         tensor_2D_b.to("cpu").detach().numpy(),
         data_range=data_range)
+    
+    
+def compare(noisy_4d, clean_4d):
+    clean_np = clean_4d.squeeze(-1).squeeze(0).detach().cpu().numpy()
+    noisy_np = noisy_4d.squeeze(-1).squeeze(0).detach().cpu().numpy()
+    
+    mse_val = ((clean_np - noisy_np) ** 2).mean()
+    psnr_val = peak_signal_noise_ratio(clean_np, noisy_np, data_range=1)
+    ssim_val = structural_similarity(clean_np, noisy_np, data_range=1)
+    
+    return mse_val, psnr_val, ssim_val

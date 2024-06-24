58cf800d8626acc4f17eadf9918a416832ae7212
trung-vt <trung-vt>
2024-06-24 12:57:44 -0500

    extract test config

---
+++ turtle_id/test/turtle_id_test_config.py
@@ -0,0 +1,18 @@
+upper_level = ".."
+
+data_path=f"{upper_level}/../data/turtle_id_2022/turtles-data/data"
+
+# "T": 128, "train_num_samples": 500, "val_num_samples": 50, "resize_square": 256, "sigmas": "[0.05, 0.1, 0.15, 0.2, 0.25, 0.3]", "init_filters": 32, "n_blocks": 3,
+model_path=f"{upper_level}/models/model-turtle_id_2022-2024_06_21_14_31_50/model_epoch_30.pt"
+
+
+# test_reg_map_path = "../test_results/test_reg_map"
+test_reg_map_path = "test_reg_map"
+
+# test_scalar_reg_path = "../test_results/test_scalar_reg"
+test_scalar_reg_path = "test_scalar_reg"
+
+size = 256
+num_samples = 5 # 0 for all
+
+sigmas = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3]
